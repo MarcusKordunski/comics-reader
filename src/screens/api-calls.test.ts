b4c6d5c45d412ec1c93695892883ccc797b52920
MarcusKordunski <marikord26@gmail.com>
2023-05-28 18:28:03 +0200

    clean up

--- src/screens/api-calls.test.ts
+++
@@ -1,7 +0,0 @@
-test('данные являются арахисовым маслом', async () => {
-    await expect(fetchData()).resolves.toBe('арахисовое масло');
-});
-
-test('fetch вернёт ошибку', async () => {
-    await expect(fetchData()).rejects.toMatch('error');
-});
